c7d56f965da7e80bc18fc2944a51ef7854647aa8
starlightcandy <uta9784@gmail.com>
2016-04-20 14:24:49 +0900

    post-it post dialogue is working.

--- html/script/post-it.js
+++ html/script/post-it.js
@@ -1,58 +1,35 @@
-
-/*
-var Board = function( selector ) {
-  var $elem = $( selector );
-
-  function initialize() {
-    $elem.on("click", newPostIt);
-  };
-
-  initialize();
-};
-*/
-/*
-var PostIt = function() {
-
-  function initialize() {
-    $("#board_wrapper").append('<div id="card_dial"><input></input><div id="c_wrapper"><div id ="c1"></div><div id ="c2"></div><div id ="c3"></div><div id ="c4"></div><div id ="c5"></div></div><div id="submit"></div></div>')
-    $("#card_dial").css({'top': event.pageY, 'left': event.pageX});
-  };
-
-  function deletePostIt(e){
-    e.stopPropagation();
-    var $parent = $(this.parentElement.parentElement);
-    $parent.remove();
-  };
-
-  initialize();
-};
-*/
 var setclick = true;
 var key_content;
 
+function remove_dial(){
+  $('#overlay').fadeOut(300, function(){ $(this).remove();});
+  $('#card_dial').fadeOut(300, function(){ $(this).remove();});
+  setclick=true;
+}
+
+function fo(){
+  $('#anchor').find('#submit_key').click(function(){
+    if(!setclick){
+      key_content = $('#card_dial input').val();
+      remove_dial();
+    }
+  });
+
+  $('#anchor').find('#overlay').click(function(){
+    if(!setclick){
+      setclick=true;
+      remove_dial();
+    }
+  });
+}
+
 $(document).ready(function () {
-
-      $('#submit_key').on('click', function(e){
-        key_content = $('#card_dial input').val();
-        $('#overlay').fadeOut('slow');
-        setclick=true;
-      });
-
     $('#board_wrapper').on('click', function(e){
       if (setclick){
-        $('#anchor').append('<div id="overlay"><div id="card_dial" style="top:'+e.pageY+'px; left:'+e.pageX+'px;"><input></input><div id="c_wrapper"><div id ="c1"></div><div id ="c2"></div><div id ="c3"></div><div id ="c4"></div><div id ="c5"></div></div><div id="submit_key">submit</div></div></div>');
+        $('#anchor').append('<div id="card_dial" style="top:'+e.pageY+'px; left:'+e.pageX+'px;"><input></input><div id="c_wrapper"><div id ="c1"></div><div id ="c2"></div><div id ="c3"></div><div id ="c4"></div><div id ="c5"></div></div><div id="submit_key">submit</div></div><div id="overlay"></div>');
         setclick=false;
       }
+      fo();
+
     });
-
 });
-/*
-$(function() {
-  new Board('#board_wrapper');
-});
-
-function newPostIt() {
-  new PostIt;
-  postNumber += 1;
-};
-*/
