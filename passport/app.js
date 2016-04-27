109e624870d74d146a76577be8bf3853223231ca
sh92 <sanghee.lee1992@gmail.com>
2016-04-27 21:31:54 +0900

    mongodb 연동후 login 회원가입 추가

---
+++ passport/app.js
@@ -0,0 +1,51 @@
+// dependencies
+var fs = require('fs');
+var http = require('http');
+var express = require('express');
+var routes = require('./routes');
+var path = require('path');
+var mongoose = require('mongoose');
+var path = require('path');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// global config
+
+var app = express();
+app.set('port', process.env.PORT || 1337);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.favicon());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.configure('development', function(){
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+        app.use(express.errorHandler());
+});
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+// // mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose');
+// // routes
+require('./routes')(app);
+
+app.listen(app.get('port'), function(){
+   console.log(("Express server listening on port " + app.get('port')))
+ });
+
