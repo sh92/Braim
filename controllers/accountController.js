0a54e07f81783daa8007b8f9d02bcbeb19086ed1
snkim <snkim@cs-cnu.org>
2016-05-04 18:55:53 +0900

    add register/login function

---
+++ controllers/accountController.js
@@ -0,0 +1,45 @@
+var Account = require('../model/account_model')
+  , bodyParser = require('body-parser')
+  , passport = require("passport");
+
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        // console.log(req.user); //for checking login function
+		res.render('index');
+	});
+
+    app.get('/register', function(req, res) {
+        res.render('register', {});
+    });
+
+    app.post('/register', function(req, res, next) {
+        console.log('registering user');
+        Account.register(new Account({
+            username: req.body.username
+        }), req.body.password, function(err) {
+            if (err) {
+                console.log('error while user register!', err);
+                return next(err);
+            }
+
+            console.log('user registered!');
+
+            res.redirect('/');
+        });
+    });
+
+    app.get('/login', function(req, res) {
+        res.render('login', {
+            user: req.user
+        });
+    });
+
+    app.post('/login', passport.authenticate('local'), function(req, res) {
+        res.redirect('/');
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+}
