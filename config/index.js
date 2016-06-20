c65fbfee241807b707fd86590447dcfee711a0b3
sh92 <sanghee.lee1992@gmail.com>
2016-06-20 18:25:48 +0900

    Refacotirng

--- config/index.js
+++
@@ -1,8 +0,0 @@
-/* 현재 사용하지 않는 코드로 나중에 필요할 때 문법을 가져와서 사용 (추후 삭제 예정)
-var configValue = require('./config');
-
-module.exports = {
-    getDbConnectionString: function () {
-        return "mongodb://"+configValue.uname+":"+configValue.pwd+"@ds013162.mlab.com:13162/ideadb";
-    }
-}*/
